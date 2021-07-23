5896e70e98c3e2d3a6b59a3e5c94df9445628dbe
Prabal2207 <prabal760@gmail.com>
2021-07-23 13:29:04 +0530

    [Prabal] Refactor1:-Generic impletation at Class level and printing max  for maximum of more than 3

--- MaximumNumber/MaximumNumber/Program.cs
+++ MaximumNumber/MaximumNumber/Program.cs
@@ -8,15 +8,26 @@ namespace MaximumNumber
         {
             Console.WriteLine("Welcome to Generic Implementation");
 
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
+            generic.PrintMaxValue();
+            double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
+            GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
+            genericDouble.PrintMaxValue();
+            string[] stringArray = { "111", "222", "333", "55", "999" };
+            GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
+            //GenericMaximum<Employee> genericString = new GenericMaximum<string>(stringArray);
+            genericString.PrintMaxValue();
 
-            string maximum = MaximumNumberCheck.FindMaximum<int>(12, 16, 14);
-            string maximum1 = MaximumNumberCheck.FindMaximum<float>(111.2f,22.45f,78.56f);
-            string maximum2 = MaximumNumberCheck.FindMaximum<string>("22", "33", "44");
 
-            Console.WriteLine(maximum);
-            Console.WriteLine(maximum1);
-            Console.WriteLine(maximum2);
-            Console.ReadKey();
+            //string maximum = MaximumNumberCheck.FindMaximum<int>(12, 16, 14);
+            //string maximum1 = MaximumNumberCheck.FindMaximum<float>(111.2f,22.45f,78.56f);
+            //string maximum2 = MaximumNumberCheck.FindMaximum<string>("22", "33", "44");
+
+            //Console.WriteLine(maximum);
+            //Console.WriteLine(maximum1);
+            //Console.WriteLine(maximum2);
+            //Console.ReadKey();
 
             //Console.WriteLine("Find max Number");
             //int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
