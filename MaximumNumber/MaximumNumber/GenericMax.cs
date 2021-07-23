17054ea33a50cb42eda2ebff03d54e20c24431c0
Prabal2207 <prabal760@gmail.com>
2021-07-23 13:27:12 +0530

    [Prabal] Refactor1:-Generic impletation at Class level for maximum of more than 3

---
+++ MaximumNumber/MaximumNumber/GenericMax.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace MaximumNumber
+{
+    public class GenericMaximum<T> where T : IComparable //Where T data type should inherit IComparable
+    {
+        public T[] value;
+        public GenericMaximum(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+
+        }
+
+
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+
+            return sorted_values[sorted_values.Length - 1];
+        }
+
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
+    }
+
+
+}
