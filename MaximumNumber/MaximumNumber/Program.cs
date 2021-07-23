bd250291b07d38e19522eec5657415c3e7ee150e
Prabal2207 <prabal760@gmail.com>
2021-07-23 12:41:19 +0530

    [Prabal] Added:-Maximum of 3 Float is found

--- MaximumNumber/MaximumNumber/Program.cs
+++ MaximumNumber/MaximumNumber/Program.cs
@@ -12,6 +12,8 @@ namespace MaximumNumber
             Console.WriteLine("Find max Number");
             int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
             Console.WriteLine(output);
+            double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
+            Console.WriteLine(doubleoutput);
 
             Console.ReadKey();
         }
