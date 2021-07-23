a5649960733923b46b362f8afaaee4a7ad931632
Prabal2207 <prabal760@gmail.com>
2021-07-23 12:35:19 +0530

    [Prabal] Added:-Maximum of 3 integer is found

--- MaximumNumber/MaximumNumber/Program.cs
+++ MaximumNumber/MaximumNumber/Program.cs
@@ -7,6 +7,13 @@ namespace MaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generic Implementation");
+
+
+            Console.WriteLine("Find max Number");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
+            Console.WriteLine(output);
+
+            Console.ReadKey();
         }
     }
 }
